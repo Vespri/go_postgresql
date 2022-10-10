426fcf0e7fa8530444ed898654bde76454d86ada
Vespri <kresna.vespri21@gmail.com>
2022-10-10 10:39:39 +0700

    add go_sql

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "test"
+	dbname   = "db-go"
+)
+
+var (
+	db  *sql.DB
+	err error
+)
+
+func main() {
+	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err = sql.Open("postgres", sqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to database")
+}
